03e315d1ca9b767b6052018263252ddb562949e5
xiaoyufei <915724526@qq.com>
2018-11-02 08:17:57 +0800

    edit test name to template

--- test/run.py
+++
@@ -1,5 +0,0 @@
-# coding:utf-8
-
-# 将内容存取到item.json文件中
-from scrapy import cmdline
-cmdline.execute("scrapy crawl review -o item.json".split())
--- test/test/items.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import scrapy
-
-class DmozItem(scrapy.Item):
-    title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
